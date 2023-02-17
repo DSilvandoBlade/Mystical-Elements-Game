6621c457f39c5d0935fa875a07a531efde03573a
DSilvandoBlade <dannyfromsudo@gmail.com>
2023-02-17 15:05:15 +0000

    element class start

--- Mystical Elements/Assets/Script/Class/ElementClass.cs
+++ Mystical Elements/Assets/Script/Class/ElementClass.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ElementClass : MonoBehaviour
+[System.Serializable]
+public static class ElementClass
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum Element
     {
-        
+        DEFAULT = 0,
+        PLASMA,
+        FROST,
+        CHARGE,
+        FLORA
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
